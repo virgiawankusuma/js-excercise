41ccbb0bfeaf14e9e4825f3ab7427cc139e2ae4c
virgiawankusuma <virgiawan150@gmail.com>
2021-12-19 23:41:43 +0700

    feat: DOM Manipulation: innerHTML

---
+++ 2. DOM Manipulation/script.js
@@ -0,0 +1,7 @@
+// DOM Manipulation
+// // innerHTML
+const judul = document.getElementById('judul');
+judul.innerHTML = '<em>Virgiawan</em>';
+
+const sectionA = document.querySelector('section#a');
+sectionA.innerHTML = '<div><p>Paragraf 1</p></div>';
