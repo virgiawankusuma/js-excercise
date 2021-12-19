772c8b205717a65d44909f6d190af8883c85c7e0
virgiawankusuma <virgiawan150@gmail.com>
2021-12-19 22:48:04 +0700

    feat: DOM Selection: getElementById

---
+++ 1. DOM Selection/script.js
@@ -0,0 +1,6 @@
+// DOM Selection
+// // getElementById
+const judul = document.getElementById('judul');
+judul.style.color = 'red';
+judul.style.backgroundColor = '#ccc';
+judul.innerHTML = 'Virgiawan';
