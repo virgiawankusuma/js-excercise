ef3d63166a4bf57eb7016fdb90a145b8ae7b0ce8
virgiawankusuma <virgiawan150@gmail.com>
2021-12-19 22:56:01 +0700

    feat: DOM Selection: getElementsByClassName

--- 1. DOM Selection/script.js
+++ 1. DOM Selection/script.js
@@ -15,3 +15,7 @@ for (let i = 0; i < p.length; i++) {
 
 const h1 = document.getElementsByTagName('h1')[0];
 h1.style.fontSize = '50px';
+
+// // getElementsByClassName -> HTMLCollection
+const p1 = document.getElementsByClassName('p1');
+p1[0].innerHTML = 'Ini diubah dari javascript';
