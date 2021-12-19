8f3f1a7ecb64243584f098f0fe38943d384b94be
virgiawankusuma <virgiawan150@gmail.com>
2021-12-19 14:50:24 +0700

    feat: Making Object: Object Literals

---
+++ 1. Making Object/script.js
@@ -0,0 +1,22 @@
+// Making Object
+// // Object Literals
+const mhs1 = {
+    nama: 'Virgiawan Teguh Kusuma',
+    umur: 21,
+    ips: [3.48, 3.75, 3.86, 3.7, 3.78, 3.95],
+    alamat: {
+        jalan: 'Jl. xzy No.123',
+        kota: 'Jepara',
+        provinsi: 'Jawa Tengah'
+    }
+}
+const mhs2 = {
+    nama: 'Muhamad Ilyas',
+    umur: 21,
+    ips: [3.8, 3.9, 3.9, 3.92, 3.95, 3.99],
+    alamat: {
+        jalan: 'Jl. abs No.321',
+        kota: 'Jepara',
+        provinsi: 'Jawa Tengah'
+    }
+}
