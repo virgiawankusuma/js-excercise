021d06772f145ab50c0473a8d7cdf4552db4d99d
virgiawankusuma <virgiawan150@gmail.com>
2021-12-19 22:54:22 +0700

    feat: DOM Selection: getElementsByTagName

--- 1. DOM Selection/script.js
+++ 1. DOM Selection/script.js
@@ -1,6 +1,17 @@
 // DOM Selection
-// // getElementById
+// // getElementById -> element
 const judul = document.getElementById('judul');
 judul.style.color = 'red';
 judul.style.backgroundColor = '#ccc';
 judul.innerHTML = 'Virgiawan';
+
+// // getElementsByTagName() -> HTMLCollection
+const p = document.getElementsByTagName('p');
+// p[0].style.backgroundColor = 'lightblue';
+
+for (let i = 0; i < p.length; i++) {
+    p[i].style.backgroundColor = 'lightblue';
+}
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '50px';
