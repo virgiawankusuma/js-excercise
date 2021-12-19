2db36b412fcb7930375094903fb043d14f5db36a
virgiawankusuma <virgiawan150@gmail.com>
2021-12-19 23:44:09 +0700

    feat: DOM Manipulation: style

--- 2. DOM Manipulation/script.js
+++ 2. DOM Manipulation/script.js
@@ -1,7 +1,12 @@
 // DOM Manipulation
 // // innerHTML
-const judul = document.getElementById('judul');
-judul.innerHTML = '<em>Virgiawan</em>';
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<em>Virgiawan</em>';
 
-const sectionA = document.querySelector('section#a');
-sectionA.innerHTML = '<div><p>Paragraf 1</p></div>';
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = '<div><p>Paragraf 1</p></div>';
+
+// // style
+const judul = document.querySelector('#judul');
+judul.style.color = 'lightblue';
+judul.style.backgroundColor = 'salmon';
