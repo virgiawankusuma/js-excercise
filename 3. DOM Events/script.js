baf1a9aa3c6a440c6d01549721503893ed165cb0
virgiawankusuma <virgiawan150@gmail.com>
2021-12-20 11:25:52 +0700

    feat: DOM Events: Event Handler: Element method

--- 3. DOM Events/script.js
+++ 3. DOM Events/script.js
@@ -2,6 +2,15 @@
 // // Event Handler
 const p3 = document.querySelector('.p3');
 
-function ubahWarna() {
+function ubahWarnaP3() {
     p3.style.backgroundColor = 'lightblue';
 }
+
+// // // Element method
+const p2 = document.querySelector('.p2');
+
+function ubahWarnaP2() {
+    p2.style.backgroundColor = 'lightblue';
+}
+
+p2.onclick = ubahWarnaP2;
