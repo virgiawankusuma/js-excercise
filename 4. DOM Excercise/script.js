e7faaa7600d5bf9e5d0d33de28dd526a6d789d66
virgiawankusuma <virgiawan150@gmail.com>
2021-12-20 12:34:35 +0700

    feat: DOM Excercise: Add button tAcakWarna Events w/ addEventListener

--- 4. DOM Excercise/script.js
+++ 4. DOM Excercise/script.js
@@ -12,3 +12,10 @@ tAcakWarna.appendChild(textTAcakWarna);
 tAcakWarna.setAttribute('type', 'button');
 tAcakWarna.setAttribute('id', 'tAcakWarna');
 tUbahWarna.after(tAcakWarna);
+
+tAcakWarna.addEventListener('click', () => {
+    const r = Math.round(Math.random() * 255);
+    const g = Math.round(Math.random() * 255);
+    const b = Math.round(Math.random() * 255);
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+})
